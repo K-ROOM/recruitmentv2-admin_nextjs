cbcd0dcacf395a5bd29e0865fbc1fcc1d2220179
k-room <ronakorn.steam@hotmail.com>
2024-07-09 14:32:31 +0700

    Deploy

--- src/app/main/account/page.tsx
+++ src/app/main/account/page.tsx
@@ -2,13 +2,13 @@
 import { useSession, signIn, signOut } from "next-auth/react";
 import { redirect } from "next/navigation";
 import { getServerSession } from "next-auth";
-import PermissionDenied from "@/app/components/permission_denied";
+import PermissionDenied from "src/app/components/permission_denied";
 import { Suspense } from "react";
-import Loading from "@/app/loading";
-import MainNavbar from "@/app/components/main_navbar";
-import AccountBody from "@/app/components/account_body";
-import Login from "@/app/components/login";
-import { authOptions } from "@/app/api/auth/[...nextauth]/auth";
+import Loading from "src/app/loading";
+import MainNavbar from "src/app/components/main_navbar";
+import AccountBody from "src/app/components/account_body";
+import Login from "src/app/components/login";
+import { authOptions } from "src/app/api/auth/[...nextauth]/auth";
 
 export default async function Account() {
   const session = await getServerSession(authOptions)
