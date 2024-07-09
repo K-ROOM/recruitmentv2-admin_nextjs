b6a4bde0e00554f5765d247afd378ac63bf5194b
k-room <ronakorn.steam@hotmail.com>
2024-07-09 14:14:59 +0700

    lko

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,4 @@
 /** @type {import('next').NextConfig} */
-
-import path from 'path';
-
-const __dirname = new URL('.', import.meta.url).pathname;
-
 const nextConfig = {
     images: {
         domains: ['lh3.googleusercontent.com'],
@@ -13,13 +8,6 @@ const nextConfig = {
     },
     reactStrictMode: true,
     // useFileSystemPublicRoutes: false,
-    webpack: (config) => {
-        config.resolve.alias = {
-            ...config.resolve.alias,
-            '@': path.resolve(__dirname, './'),
-        };
-        return config;
-    },
 };
 
 export default nextConfig;
